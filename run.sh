ba41e4e5094c637d98ce97e87e6189e11cb075d7
Jeffrey Salmond <js947@cam.ac.uk>
2018-11-19 14:04:04 +0000

    move running environment out of run.sh

--- run.sh
+++ run.sh
@@ -6,12 +6,7 @@
 #SBATCH --exclusive
 #SBATCH -t 12:00:00 
 
-module purge
-module load slurm
-module load cuda/9.0 cudnn/7.3_cuda-9.0
-source env/bin/activate
-export LD_LIBRARY_PATH=$HOME/tf-dist/nccl_2.2.13-1+cuda9.0_x86_64/lib:$LD_LIBRARY_PATH
-export NCCL_DEBUG=INFO
+source runenv
 
 if [[ $SLURM_NNODES -gt 1 ]]
 then
